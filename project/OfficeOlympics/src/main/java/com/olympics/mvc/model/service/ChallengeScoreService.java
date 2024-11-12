b7a7b8735bd5c1b318eb667a3551b6ba2caeeb82
yes24150 <yes24150@gmail.com>
2024-11-12 17:47:54 +0900

    feat: 챌린지 관련 기능 추가
    챌린지 조회, 챌린지 결과 기록, 챌린지 점수 업데이트 기능을 구현했습니다.
    
    - 현재 챌린지에서의 순위 조회 및 올림픽 팀 내 순위 조회 기능 추가
    
    - 리더보드에서 순위 조회 기능을 구현하여 전체 순위 파악 가능

--- project/OfficeOlympics/src/main/java/com/olympics/mvc/model/service/ChallengeScoreService.java
+++ project/OfficeOlympics/src/main/java/com/olympics/mvc/model/service/ChallengeScoreService.java
@@ -1,5 +1,28 @@
 package com.olympics.mvc.model.service;
 
-public interface ChallengeScoreService {
+import java.util.List;
 
+import com.olympics.mvc.model.dto.Challenge;
+import com.olympics.mvc.model.dto.Rank;
+import com.olympics.mvc.model.dto.Score;
+
+public interface ChallengeScoreService {
+	
+	// 챌린지 조회
+    Challenge selectChallenge(int challengeId);
+    
+    // 챌린지 결과 기록
+    boolean insertScores(Score score);
+    
+    // 챌린지 점수 업데이트
+    void updateScore();
+    
+    // 현재 챌린지에서의 순위 조회
+    List<Rank> selectChallengeScore(int challengeId);
+    
+    // 올림픽 팀 내 순위 조회
+    List<Rank> selectFinalScore(int olympicsId);
+    
+    // 리더보드에서 순위 조회
+    List<Rank> selectMainScore();
 }
