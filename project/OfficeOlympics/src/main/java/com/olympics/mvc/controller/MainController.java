db389404af65a88523b7ce5d48987f2d6447a726
EunSeo Yang <yes24150@gmail.com>
2024-11-21 23:15:30 +0900

    refactor: 파일 업로드 시 UUID 추가 및 전반적인 리팩토링
    파일 업로드 시 고유성을 보장하기 위해 UUID를 추가했습니다. 코드 구조를 개선하여 가독성과 유지보수성을 향상시켰습니다.
    
    - 파일 저장 로직 최적화 - 중복 코드 제거 및 함수 분리

--- project/OfficeOlympics/src/main/java/com/olympics/mvc/controller/MainController.java
+++ project/OfficeOlympics/src/main/java/com/olympics/mvc/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.olympics.mvc.controller;
 
-import java.io.File;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
--- project/OfficeOlympics/src/main/java/com/olympics/mvc/model/service/UserServiceImpl.java
+++ project/OfficeOlympics/src/main/java/com/olympics/mvc/model/service/UserServiceImpl.java
@@ -6,8 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.time.LocalDate;
-import java.util.List;
+import java.util.UUID;
 
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
@@ -29,45 +28,45 @@ public class UserServiceImpl implements UserService{
 		this.userDao = userDao;
 	}
 	
-	///////////////////////////////////회원가입///////////////////////////////////
+	private final String PREFIX = "http://localhost:8080/uploads/profile/";
+	private final String IMG_PATH = "src/main/resources/static/uploads/profile";
 	
-	// 회원가입 로직
+	/**
+	 * 회원가입
+	 */
 	@Transactional
 	@Override
 	public boolean insertUser(User user, MultipartFile profileImg) {
-		// 비밀번호 암호화
 		String salt = HashUtil.generateSalt();
 		user.setSalt(salt);
 		String hashedPassword = HashUtil.hashPassword(user.getPassword(), salt);
 		user.setPassword(hashedPassword);
 		
-		// 파일이 있으면 저장
 		if (profileImg != null && profileImg.getSize() > 0) {
 			try {
-				// 확장자 검증
+
 				if (!FileConfirm.isValidImageFile(profileImg)) {
 				    throw new IllegalArgumentException("jpg 또는 png 파일만 업로드 가능합니다.");
 				}
 				
 				String orgImg = profileImg.getOriginalFilename();
-				String img = LocalDate.now()+"_"+orgImg;
+				String uuid = UUID.randomUUID().toString().replace("-", "");
+				String uuidImgName = uuid+"_"+orgImg;
 				
-				String staticPath = "src/main/resources/static/uploads/profile";
-				File uploadDir = new File(staticPath);
+				File uploadDir = new File(IMG_PATH);
 				
 				if(!uploadDir.exists()) {
 					uploadDir.mkdirs();
 				}
 				
-				Path filePath = Paths.get(staticPath, img);
+				Path filePath = Paths.get(IMG_PATH, uuidImgName);
 				
 				try (InputStream inputStream = profileImg.getInputStream()) {
 				    Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
 				}
 				
-				String prefix = "http://localhost:8080/uploads/profile/";
-				user.setProfileImg(prefix+img);
-				user.setImgSrc(orgImg);
+				user.setProfileImg(orgImg);
+				user.setImgSrc(PREFIX+uuidImgName);
 				
 			} catch (Exception e){
 				e.printStackTrace();
@@ -81,25 +80,27 @@ public class UserServiceImpl implements UserService{
 		return res == 1;
 	}
 	
-	// 회원 탈퇴
+	/**
+	 * 회원탈퇴
+	 */
 	@Transactional
 	@Override
 	public boolean deleteUser(int id) {
 		int res = userDao.deleteUser(id);
 		return res == 1;
 	}
-	
-	
-	
-	///////////////////////////////////로그인///////////////////////////////////
-	
-	// 로그인 로직
+
+	/**
+	 * 로그인
+	 */
 	@Override
 	public User selectUser(String email) {
 		return userDao.selectUser(email);
 	}
 	
-	// 비밀번호 확인 메서드
+	/**
+	 * 비밀번호 확인
+	 */
 	public boolean checkPassword(User user, String inputPassword) {
 		// 사용자의 솔트를 가져와서 입력된 비밀번호를 해싱
 		String hashedInputPassword = HashUtil.hashPassword(inputPassword, user.getSalt()); // 입력된 비밀번호 해싱
@@ -108,50 +109,48 @@ public class UserServiceImpl implements UserService{
 		return hashedInputPassword.equals(user.getPassword());
 	}
 
-	
-	///////////////////////////////////마이페이지///////////////////////////////////
-	
-	
-	// id index로 회원 조회 (마이페이지 조회)
+	/**
+	 * id index로 회원 조회 (마이페이지 조회)
+	 */
 	@Override
 	public User selectById(int id) {
 		return userDao.selectById(id);
 	}
 
-	// 회원 정보 수정
+	/**
+	 * 회원정보 수정
+	 */
 	@Transactional
 	@Override
 	public boolean modifyUser(User user, MultipartFile profileImg) {
-		// 기존 사용자 정보 조회
+		
 		User exUser = userDao.selectById(user.getUserId());
-		System.out.println(exUser);
-		// 파일이 있으면 저장
+		
 		if (profileImg != null && profileImg.getSize() > 0) {
 			try {
-				// 확장자 검증
+				
 				if (!FileConfirm.isValidImageFile(profileImg)) {
 					throw new IllegalArgumentException("jpg 또는 png 파일만 업로드 가능합니다.");
 				}
 				
 				String orgImg = profileImg.getOriginalFilename();
-				String img = LocalDate.now()+"_"+orgImg;
+				String uuid = UUID.randomUUID().toString().replace("-", "");
+				String uuidImgName = uuid+"_"+orgImg;
 				
-				String staticPath = "src/main/resources/static/uploads/profile";
-				File uploadDir = new File(staticPath);
+				File uploadDir = new File(IMG_PATH);
 				
 				if(!uploadDir.exists()) {
 					uploadDir.mkdirs();
 				}
 				
-				Path filePath = Paths.get(staticPath, img);
+				Path filePath = Paths.get(IMG_PATH, uuidImgName);
 				
 				try (InputStream inputStream = profileImg.getInputStream()) {
 				    Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
 				}
 				
-				String prefix = "http://localhost:8080/uploads/profile/";
-				user.setProfileImg(prefix+img);
-				user.setImgSrc(orgImg);
+				user.setProfileImg(orgImg);
+				user.setImgSrc(PREFIX+uuidImgName);
 				
 				
 			} catch (Exception e){
@@ -159,25 +158,16 @@ public class UserServiceImpl implements UserService{
 				return false;	
 			}
 		} else {
-			// 이미지 파일이 없으면 기존 이미지 데이터 유지
+			
 			user.setProfileImg(exUser.getProfileImg());
 			user.setImgSrc(exUser.getImgSrc());
 		}
 		
-		// 기타 정보 업데이트 (빈 값이 덮어쓰지 않도록 처리)
 		if (user.getName() == null) user.setName(exUser.getName());
 		if (user.getEmail() == null) user.setEmail(exUser.getEmail());
 		
-		// 사용자 정보 업데이트
 		int res = userDao.modifyUser(user);
 		return res == 1;
 	}
-	
-	
-	// 전체 계정 정보 조회
-	@Override
-	public List<User> selectAccounts() {
-		return userDao.selectAccounts();
-	}
-	
+		
 }
