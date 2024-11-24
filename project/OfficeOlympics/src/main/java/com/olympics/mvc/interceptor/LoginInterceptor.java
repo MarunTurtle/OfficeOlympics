30c368b2c8ef8508362507820014d8c47e49205c
yes24150 <yes24150@gmail.com>
2024-11-25 08:59:36 +0900

    feat: 로그인 인터셉터 추가
    로그인 상태를 검증하기 위한 인터셉터를 추가했습니다.
    
    인증이 필요한 요청에서 세션이나 토큰 유효성을 확인하도록 구현했습니다.

--- project/OfficeOlympics/src/main/java/com/olympics/mvc/interceptor/LoginInterceptor.java
+++ project/OfficeOlympics/src/main/java/com/olympics/mvc/interceptor/LoginInterceptor.java
@@ -1,8 +1,30 @@
 package com.olympics.mvc.interceptor;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class LoginInterceptor implements HandlerInterceptor{
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+@Configuration
+public class LoginInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+	    System.out.println("Session ID: " + session.getId());
+	    System.out.println("loginUserId: " + session.getAttribute("loginUserId"));
+	    
+	    Integer isLogin = (Integer) session.getAttribute("loginUserId");
+	    if (isLogin == null) {
+	        System.out.println("No loginUserId found");
+	        return false;
+	    }
+
+	    System.out.println("User logged in with ID: " + isLogin);
+	    return true;
+	}
 	
 }
